d3f6a8f868203b4f0870a167f325a7a63ae21cb2
HassaElgallouchi <helgallouchi@gmail.com>
2021-02-21 17:04:07 +0100

    add button read more article

--- js/index.js
+++ js/index.js
@@ -1,18 +1,21 @@
 // import elements of dom
-let section = document.querySelector('main .container section');
+let section = document.querySelector('main .container section .article');
+let btn = document.querySelector('.btn');
 console.log(section);
 
+// create function async to fetch a url that return a promise
 let getData = async (url) => {
     let response = await fetch(url);
     return await response.json();
 }
 
+// function to generate the random image color
 let generateRandomColor = () => {
-    let colors = ['16a085','27ae60','2c3e50','f39c12','e74c3c','9b59b6','FB6964','342224','472E32','BDBB99','77B1A9','73A857'];
+    let colors = ['16a085', '27ae60', '2c3e50', 'f39c12', 'e74c3c', '9b59b6', 'FB6964', '342224', '472E32', 'BDBB99', '77B1A9', '73A857'];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     return randomColor;
 }
-console.log(generateRandomColor());
+
 (async () => {
     section.innerHTML = 'loading...';
 
@@ -21,7 +24,8 @@ console.log(generateRandomColor());
     if (posts.length > max) {
         posts = posts.slice(0, max)
     }
-section.innerHTML = "";
+
+    section.innerHTML = "";
     posts.forEach(post => {
         section.innerHTML += `
         <article>
@@ -31,11 +35,12 @@ section.innerHTML = "";
             </a>
         </div>
         <div class="content-article">
-            <h2>${post.id}- 
+            <h2> 
                 <a href="/article.html?id=${post.id}">${post.title}</a>
             </h2>
         </div>
     </article>`;
     });
-
 })();
+
+
